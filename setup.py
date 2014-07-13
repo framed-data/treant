7935d959ebbc6c1d3bf2f86aa7e9fe6f8e482ccb
Elliot Block <elliot@deck36.net>
2014-07-13 15:59:36 -0700

    Add setup.py metadata
    This'll allow us to install to PyPi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "treant",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = [],
+    author = "Elliot Block",
+    author_email = "elliot@framed.io",
+    description = "Simple tree operations in Python",
+    url = "https://github.com/framed-data/treant",
+)
